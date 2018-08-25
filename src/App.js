66bdd9de6ea592ceb0d85350f31713ab327db706
zenott <mblevel@gmail.com>
2018-08-25 23:21:46 +0200

    Add more styles

--- src/App.js
+++ src/App.js
@@ -12,31 +12,27 @@ class App extends Component {
             <div id="settings-left">
               <div id="break-label">Break Length</div>
               <div id="break-settings">
-                <div id="break-decrement"><i className="fas fa-arrow-down"></i></div>
-                <div id="break-length"></div>
-                <div id="break-increment"><i className="fas fa-arrow-up"></i>
-
-</div>  
+                <div id="break-decrement"><i className="fas fa-arrow-down fa-2x"></i></div>
+                <div id="break-length">5</div>
+                <div id="break-increment"><i className="fas fa-arrow-up fa-2x"></i></div>  
               </div>
             </div>
             <div id="settings-right">
               <div id="session-label">Session Length</div>
               <div id="session-settings">
-                <div id="session-decrement"><i className="fas fa-arrow-down"></i></div>
-                <div id="session-length"></div>
-                <div id="session-increment"><i className="fas fa-arrow-up"></i>
-
-</div> 
+                <div id="session-decrement"><i className="fas fa-arrow-down fa-2x"></i></div>
+                <div id="session-length">25</div>
+                <div id="session-increment"><i className="fas fa-arrow-up fa-2x"></i></div> 
               </div>
             </div>
           </div>
           <div id="display">
             <div id="timer-label">Session</div>
-            <div id="time-left"></div>
+            <div id="time-left">25:00</div>
           </div>
           <div id="controls">
-            <div id="start-stop"><i class="fas fa-play"></i><i class="fas fa-pause"></i><i class="fas fa-sync"></i></div>
-            <div id="reset"></div>
+            <div id="start-stop"><i class="fas fa-play fa-2x"></i><i class="fas fa-pause fa-2x"></i></div>
+            <div id="reset"><i class="fas fa-sync fa-2x"></i></div>
           </div>
         </div>
       </div>
