40e1fe23b0ce7974a1183ae2d319bd05461a0b5a
zenott <mblevel@gmail.com>
2018-08-27 15:08:31 +0200

    Add countdown timer

--- src/App.js
+++ src/App.js
@@ -7,25 +7,57 @@ class App extends Component {
     super(props)
     this.state={
       session:25,
-      break:5
+      break:5,
+      countDown:0,
+      isOn: false
     }
+    this.timer=0;
     this.handleClick = this.handleClick.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
   }
   handleClick(e){
-    if(e.target.id==='seDec'){
+    if(e.target.id==='seDec' && this.state.session>1){
       this.setState({session:this.state.session-1});
     }
-    if(e.target.id==='seInc'){
+    if(e.target.id==='seInc' && this.state.session<60){
       this.setState({session:this.state.session+1});
     }
-    if(e.target.id==='brDec'){
+    if(e.target.id==='brDec' && this.state.break>1){
       this.setState({break:this.state.break-1});
     }
-    if(e.target.id==='brInc'){
+    if(e.target.id==='brInc' && this.state.break<60){
       this.setState({break:this.state.break+1});
     }
+    if((e.target.id==='stSt' || e.target.id==='stSt2') && !this.state.isOn){
+      if(this.state.countDown===0){
+        this.startTimer(this.state.session*60);
+      } else{
+        this.startTimer(-1);
+      }
+      this.setState({isOn: true});
+    }
+    if((e.target.id==='stSt' || e.target.id==='stSt2') && this.state.isOn){
+      this.stopTimer();
+      this.setState({isOn: false});
+    }
+    if(e.target.id==='res'){
+      this.stopTimer();
+      this.setState({countDown:this.state.session*60, isOn: false});
+    }
+  }
+  startTimer(init){
+    if(init>0){
+      this.setState({countDown:init});
+    }
+    this.timer=setInterval(()=>(this.setState({countDown:this.state.countDown-1})),1000)
+  }
+  stopTimer(){
+    clearInterval(this.timer);
   }
   render() {
+    let min=(this.state.countDown!==0) ? Math.floor(this.state.countDown/60) : this.state.session;
+    let sec=(this.state.countDown!==0) ? this.state.countDown-60*min : '00';
     return (
       <div className="App">
         <div id="container">
@@ -50,10 +82,10 @@ class App extends Component {
           </div>
           <div id="display">
             <div id="timer-label">Session</div>
-            <div id="time-left">25:00</div>
+            <div id="time-left">{('0'+min).slice(-2)+':'+('0'+sec).slice(-2)}</div>
           </div>
           <div id="controls">
-            <div id="start-stop" onClick={this.handleClick}><i id="stSt" className="fas fa-play fa-2x"></i><i className="fas fa-pause fa-2x"></i></div>
+            <div id="start-stop" onClick={this.handleClick}><i id="stSt" className="fas fa-play fa-2x"></i><i id="stSt2" className="fas fa-pause fa-2x"></i></div>
             <div id="reset" onClick={this.handleClick}><i id="res" className="fas fa-sync fa-2x"></i></div>
           </div>
         </div>
